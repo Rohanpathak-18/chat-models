7e9ea18132b1353a59c430626f5ba0358b86b93e
rohan pathak <rohanpathak0171@gmail.com>
2026-07-31 18:14:31 +0530

    closedsource

--- 2.ChatModels/1_chatmodel_openai.py
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model = "gpt-4")
+model = ChatOpenAI(model = "gpt-4", temperature = 0.5, max_completion_tokens=10)
 
 result =  model.invoke("what is the capital of india")
 
