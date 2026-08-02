e8851f9966b0689c904bf29f83b93e1da18a7855
rohan pathak <rohanpathak0171@gmail.com>
2026-08-03 01:17:30 +0530

    messages

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,23 +8,22 @@ llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.1-8B-Instruct",
     task="text-generation"
 )
-
 model = ChatHuggingFace(llm=llm)
 
-chat_history = []
-
+chat_history = [
+  SystemMessage(content="You are a helpful assistant."),
+]
 
 
 while True:
   user_input = input("User: ")
-  
-  chat_history.append( user_input)
+  chat_history.append(HumanMessage(content=user_input))
   
   if user_input.lower() == "exit":
     break 
   
   result = model.invoke(chat_history)
-  chat_history.append( result.content)
+  chat_history.append(AIMessage(content=result.content))
   print("AI:", result.content)
   
   print(chat_history)
